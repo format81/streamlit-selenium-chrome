52b775676894e6b059746d2a70d775fff0ad14c9
Antonio Formato <44245400+format81@users.noreply.github.com>
2024-03-27 16:51:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,3 @@
-streamlit as st
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -19,13 +17,7 @@ options = Options()
 options.add_argument("--disable-gpu")
 options.add_argument("--headless")
 
-#driver = get_driver()
-#driver.get("https://blog.talosintelligence.com/lazarus_new_rats_dlang_and_telegram/")
+driver = get_driver()
+driver.get("https://blog.talosintelligence.com/lazarus_new_rats_dlang_and_telegram/")
 
-#st.code(driver.page_source)
-
-url = st.text_input("Enter the URL:")
-if url:
-    driver = get_driver()
-    driver.get(url)
-    st.code(driver.page_source)
+st.code(driver.page_source)
