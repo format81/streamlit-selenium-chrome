1d9ff73eddd4f4eaefeddef6f272ecaf2d0cb0ef
Antonio Formato <44245400+format81@users.noreply.github.com>
2024-03-27 16:58:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 
-@st.cache_resource
+#@st.cache_resource
 def get_driver():
     return webdriver.Chrome(
         service=Service(
