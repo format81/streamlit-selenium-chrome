26d4a1d455944b885e98a5e88d3be67520f5b21e
Antonio Formato <44245400+format81@users.noreply.github.com>
2024-03-27 16:47:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,24 +2,27 @@ import streamlit as st
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
+from webdriver_manager.utils import ChromeType
 
-@st.cache_resource
+@st.cache(allow_output_mutation=True)
 def get_driver():
+    options = Options()
+    options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
     return webdriver.Chrome(
-        service=Service(
-            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-        ),
+        executable_path=ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(),
         options=options,
     )
 
-options = Options()
-options.add_argument("--disable-gpu")
-options.add_argument("--headless")
+# Prompt the user to enter a URL
+url = st.text_input("Enter the URL:")
+if url:
+    # Get the webdriver instance
+    driver = get_driver()
+    driver.get(url)
 
-driver = get_driver()
-driver.get("https://blog.talosintelligence.com/lazarus_new_rats_dlang_and_telegram/")
-
-st.code(driver.page_source)
+    # Display the page source
+    st.code(driver.page_source)
+else:
+    st.warning("Please enter a URL.")
